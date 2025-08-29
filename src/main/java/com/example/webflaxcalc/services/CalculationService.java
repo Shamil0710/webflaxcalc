9d886349ba7f3acfa499ac1c16ef83d341f8d04d
Shalim <shamil0710@mail.ru>
2025-08-29 09:44:07 +0300

    комм

--- src/main/java/com/example/webflaxcalc/services/CalculationService.java
+++ src/main/java/com/example/webflaxcalc/services/CalculationService.java
@@ -2,6 +2,9 @@ package com.example.webflaxcalc.services;
 
 import reactor.core.publisher.Flux;
 
+/**
+ * Интерфейс для более четкой структуры
+ */
 public interface CalculationService {
     Flux<String> streamCsv(int count, boolean ordered);
 }
